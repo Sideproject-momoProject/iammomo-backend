07aef1e7f1d17f514b357c2a5e44fdbb0f0cb76d
seominah <seominah95@gmail.com>
2022-09-25 15:28:23 +0900

    Fix: Member 제거

--- src/main/java/dev/momo/api/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.momo.api.member.repository;
-
-import dev.momo.api.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
